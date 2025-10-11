decde4fe4568164533b86262464e5d5e24f23f74
MishaSen <mishasen245@gmail.com>
2025-10-11 16:24:57 +0800

    Add Presage Mode gameplay effect with preliminary attribute mods that grants inputstate.tb tag in blueprint

---
+++ Source/WolfCore/Private/Abilities/Effects/PresageMode.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Abilities/Effects/PresageMode.h"
+
+#include "AbilitySystem/WolfAttributeSet.h"
+#include "Core/WolfGameplayTags.h"
+#include "GameplayEffectComponents/TargetTagsGameplayEffectComponent.h"
+
+UPresageMode::UPresageMode()
+{
+	DurationPolicy = EGameplayEffectDurationType::Infinite;
+
+	FGameplayModifierInfo FlowGaugeDrain;
+	FlowGaugeDrain.Attribute = UWolfAttributeSet::GetFlowGaugeAttribute();
+	FlowGaugeDrain.ModifierOp = EGameplayModOp::Additive;
+	FlowGaugeDrain.ModifierMagnitude = FScalableFloat(-1.f);
+	Modifiers.Add(FlowGaugeDrain);
+
+	Period = 1.f;
+	bExecutePeriodicEffectOnApplication = false;
+}
