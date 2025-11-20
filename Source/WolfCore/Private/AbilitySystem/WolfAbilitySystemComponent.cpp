9d539f99548d772a386bd0d099c1bac157d8c750
MishaSen <mishasen245@gmail.com>
2025-11-20 15:18:15 +0800

    refactor: wolfabilitysystemcomponent

--- Source/WolfCore/Private/AbilitySystem/WolfAbilitySystemComponent.cpp
+++ Source/WolfCore/Private/AbilitySystem/WolfAbilitySystemComponent.cpp
@@ -12,7 +12,7 @@ void UWolfAbilitySystemComponent::AbilityInputTagPressed(const FGameplayTag& Tag
 	if (!Tag.IsValid()) return;
 
 	FScopedAbilityListLock ActiveScopeLock(*this);
-	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())
+	for (auto& AbilitySpec : GetActivatableAbilities())
 	{
 		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(Tag))
 		{
@@ -26,7 +26,7 @@ void UWolfAbilitySystemComponent::AbilityInputTagReleased(const FGameplayTag& Ta
 	if (!Tag.IsValid()) return;
 
 	FScopedAbilityListLock ActiveScopeLock(*this);
-	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())
+	for (auto& AbilitySpec : GetActivatableAbilities())
 	{
 		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(Tag) && AbilitySpec.IsActive())
 		{
@@ -40,15 +40,14 @@ void UWolfAbilitySystemComponent::AbilityInputTagHeld(const FGameplayTag& Tag)
 	if (!Tag.IsValid()) return;
 
 	FScopedAbilityListLock ActiveScopeLock(*this);
-	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())
+	for (auto& AbilitySpec : GetActivatableAbilities())
 	{
-		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(Tag))
+		if (!AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(Tag)) continue;
+
+		AbilitySpecInputPressed(AbilitySpec);
+		if (!AbilitySpec.IsActive())
 		{
-			AbilitySpecInputPressed(AbilitySpec);
-			if (!AbilitySpec.IsActive())
-			{
-				TryActivateAbility(AbilitySpec.Handle);
-			}
+			TryActivateAbility(AbilitySpec.Handle);
 		}
 	}
 }
@@ -57,12 +56,14 @@ void UWolfAbilitySystemComponent::AddCharacterAbilities(TArray<TSubclassOf<UGame
 {
 	for (const auto AbilityClass : StartupAbilities)
 	{
-		FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(AbilityClass, 1);
-		if (const UBaseCombatAbility* WolfAbility = Cast<UBaseCombatAbility>(AbilitySpec.Ability))
+		auto AbilitySpec = FGameplayAbilitySpec(AbilityClass, 1);
+		if (const auto* WolfAbility = Cast<UBaseCombatAbility>(AbilitySpec.Ability))
 		{
 			AbilitySpec.GetDynamicSpecSourceTags().AddTag(WolfAbility->StartupInputTag);
 			GiveAbility(AbilitySpec);
-			WOLF_LOG(Log, TEXT("Added ability %s for tag %s"), *AbilitySpec.Ability->GetName(), *WolfAbility->StartupInputTag.ToString());
+
+			WOLF_LOG(Log, TEXT("Added ability %s for tag %s"), *AbilitySpec.Ability->GetName(),
+			         *WolfAbility->StartupInputTag.ToString());
 		}
 	}
 }
@@ -71,21 +72,21 @@ FPresageAbilityRequest UWolfAbilitySystemComponent::BuildInitialPresageRequest(c
                                                                                const TArray<TWeakObjectPtr<AActor>>&
                                                                                Targets)
 {
-	for (const FGameplayAbilitySpec& Spec : GetActivatableAbilities())
+	for (const auto& AbilitySpec : GetActivatableAbilities())
 	{
-		if (Spec.GetDynamicSpecSourceTags().HasTagExact(Tag))
-		{
-			if (const UTBCombatAbility* TBAbility = Cast<UTBCombatAbility>(Spec.Ability))
-			{
-				return FPresageAbilityRequest(
-					this,
-					Tag,
-					0.f,
-					TBAbility->AbilitySequence,
-					Targets
-				);
-			}
-		}
+		if (!AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(Tag)) continue;
+
+		const auto* TBAbility = Cast<UTBCombatAbility>(AbilitySpec.Ability);
+		if (!TBAbility) continue;
+
+		return FPresageAbilityRequest(
+			this,
+			Tag,
+			0.f,
+			TBAbility->AbilitySequence,
+			Targets
+		);
 	}
+
 	return FPresageAbilityRequest();
 }
